12983db593e4d9067d609b133e916a74cf3e0656
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-18 00:24:05 +0530

    Place a mark in relevant place

--- LaunchGame/src/Main.java
+++ LaunchGame/src/Main.java
@@ -19,23 +19,31 @@ class TicTacToe
 
     void disBoard()
     {
-        System.out.println("------------");
+        System.out.println("-------------");
         for (int i=0; i< board.length; i++)
         {
-            System.out.print(" | ");
+            System.out.print("| ");
             for (int j=0; j<board[i].length; j++)
             {
                 System.out.print(board[i][j] + " | ");
             }
             System.out.println();
-            System.out.println("------------");
+            System.out.println("-------------");
         }
+    }
+
+    void placeMark(int row, int col, char mark)
+    {
+        board[row][col] = mark;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         TicTacToe t = new TicTacToe();
+        t.disBoard();
+        t.placeMark(0, 0, 'X');
+        t.placeMark(2, 1, 'O');
         t.disBoard();
     }
 }
