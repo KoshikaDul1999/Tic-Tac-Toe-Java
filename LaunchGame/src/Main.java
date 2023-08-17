ec8e1fc076b524591f2595185b9850696561131a
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-17 23:50:44 +0530

    Update Main.java

--- LaunchGame/src/Main.java
+++ LaunchGame/src/Main.java
@@ -1,5 +1,21 @@
+class TicTacToe
+{
+    char[][] board;
+    public TicTacToe(){
+        board = new char[3][3];
+    }
+
+    
+}
+
+
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
