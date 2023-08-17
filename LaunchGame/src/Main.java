c2b1832f9fccea832a8e838043acf14898bfe604
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-17 23:51:02 +0530

    Update Main.java

--- LaunchGame/src/Main.java
+++ LaunchGame/src/Main.java
@@ -3,9 +3,18 @@ class TicTacToe
     char[][] board;
     public TicTacToe(){
         board = new char[3][3];
+        initBoard();
     }
 
-    
+    void initBoard(){
+        for (int i=0; i< board.length; i++)
+        {
+            for (int j=0; j<board[i].length; j++)
+            {
+                board[i][j] = ' ';
+            }
+        }
+    }
 }
 
 
